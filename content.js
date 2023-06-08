9057bfc19c9cccc82371801b117b3514cbc3f6cc
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-06-08 15:19:22 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,11 @@
 
-
+// add absolute URL to links
+
+const links = document.querySelector('link');
+
+links.forEach(link => {
+  
+  link.href = chrome.runtime.getURL(link.href);
+  
+});
+
